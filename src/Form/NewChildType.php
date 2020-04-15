e19277e98612e34cfc521e6e0fd61aeb9860c21c
Nathan <SmashScharrer-Dev@outlook.fr>
2020-04-15 23:09:07 +0200

    Start New Child Form Page
    Create New "Child" Entity + Controller + Form + Repository + JS & SCSS Files
    Update Webpack Config & LoginFormaAuthenticator
    Add New Picture

---
+++ src/Form/NewChildType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Child;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewChildType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNaissance')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Child::class,
+        ]);
+    }
+}
