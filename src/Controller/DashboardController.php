6df9e4cfa1a34d1ce015df1a7ff6c43fb1b951d8
Nathan <SmashScharrer-Dev@outlook.fr>
2020-04-17 12:34:34 +0200

    Create Saving Data Method for New Child Form
    Update NewChild Controller to Add New Method for Saving Data
    Update New Child Template
    Update Child Entity (Fix Bug)
    Add Font-Awesome
    Add New Picture for New Child Form Page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Child;
+use App\Entity\User;
 use App\Form\NewChildType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
@@ -21,16 +25,38 @@ class DashboardController extends AbstractController
 
     /**
      * @Route("/dashboard/newchild", name="dashboard.newchild")
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
-    public function newChild()
+    public function newChild(Request $request)
     {
-        $child = new Child();
-        $form = $this->createForm(NewChildType::class, $child);
+        /* Create & Generate Form */
+        $child = new Child(); // Nouvelle Objet de l'Entité Child
+        $form = $this->createForm(NewChildType::class, $child); // Génération du Form à partir du fichier Form
+
+        /* Submit Form Method */
+        $form->handleRequest($request); // Récupération des Paramètres (POST/GET)
+        if($form->isSubmitted() && $form->isValid()){
+
+            $child
+                ->setNom($form->get('nom')->getData()) // Récupération Data Nom du Form
+                ->setPrenom($form->get('prenom')->getData()) // Récupération Data Prénom du Form
+                ->setDateNaissance($form->get('dateNaissance')->getData()) // Récupération Data Date de Naissance du Form
+                ->setDateInscription()
+                ->setNoUser($this->getUser())
+            ;
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($child); // Création Requête avec les données ci-dessus
+            $entityManager->flush(); // Envoi de la Requête
+
+            /* Redirection */
+            return $this->redirectToRoute('dashboard.newchild');
+
+        }
 
         return $this->render('dashboard/newchild.html.twig', [
-            'controller_name' => 'DashboardController',
             'addChildForm' => $form->createView()
         ]);
-        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- src/Form/NewChildType.php
+++ src/Form/NewChildType.php
@@ -4,17 +4,57 @@ namespace App\Form;
 
 use App\Entity\Child;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class NewChildType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('dateNaissance')
+            ->add('nom', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseignez le nom de votre enfant !'
+                    ]),
+                    new Length([
+                        'max' => '50',
+                        'maxMessage' => 'Le Nom de l\'Enfant ne peut contenir plus de {{ limit }} caractères !'
+                    ])
+                ],
+                'label' => 'Nom de l\'Enfant',
+                'required' => true
+            ])
+            ->add('prenom', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseignez le prénom de votre enfant !'
+                    ]),
+                    new Length([
+                        'max' => '50',
+                        'maxMessage' => 'Le Prénom de l\'Enfant ne peut contenir plus de {{ limit }} caractères !'
+                    ])
+                ],
+                'label' => 'Prénom de l\'Enfant',
+                'required' => true
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'widget' => 'single_text',
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseignez la date de naissance de votre enfant !'
+                    ])
+                ],
+                'label' => 'Date de Naissance de l\'Enfant',
+                'required' => true
+            ])
         ;
     }
 
