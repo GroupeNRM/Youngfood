59b3666c7cfac39fadcb5ae5220ec15e23e2bfc9
Nathan <SmashScharrer-Dev@outlook.fr>
2020-05-15 23:52:39 +0200

    Update Change Password Form

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -20,11 +20,11 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => 'Veuillez saisir un mot de passe !',
                         ]),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            'minMessage' => 'Voitre mot de passe doit contenir au minimum {{ limit }} caractères !',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
@@ -34,7 +34,7 @@ class ChangePasswordFormType extends AbstractType
                 'second_options' => [
                     'label' => 'Répétez le mot de passe',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Mot de Passe différents !',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
