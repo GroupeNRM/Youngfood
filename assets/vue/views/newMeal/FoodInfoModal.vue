5b1f71ba40a75b2db0888e6d23fbb037359b5606
Maxime <maxime.jme@gmail.com>
2020-05-16 19:00:12 +0200

    modified food & foodInfoModal component to display a modal with the info

--- assets/vue/views/newMeal/FoodInfoModal.vue
+++ assets/vue/views/newMeal/FoodInfoModal.vue
@@ -1,34 +1,26 @@
 <template>
     <div class="modal-mask">
         <div class="modal-wrapper">
-            <div class="modal-container" v-if="info">
-                // TODO
+            <div class="modal-container">
+                <div class="modal-head border-bottom mb-2">
+                    <h5>{{food.title}}</h5>
+                    <span @click="hideModal" class="modal-close">&times;</span>
+                </div>
+                <div class="modal-main">
+                    {{food.information ? food.information : "Il n'y a pas d'informations concernant cet aliment"}}
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
-
     export default {
         name: "FoodInfoModal",
-        props: ['id'],
-        mounted() {
-            this.fetchInfo(this.id);
-        },
-        data: function() {
-            return {
-                info: {}
-            }
-        },
+        props: ['food'],
         methods: {
-            fetchInfo: function (id) {
-                axios.get(`/api/food/${id}`)
-                    .then(response => this.info = response.data['hydra:member'])
-                    .catch(function() {
-                        console.log('Erreur dans le chargement!');
-                    });
+            hideModal() {
+                this.$parent.hideInformation();
             }
         }
     }
