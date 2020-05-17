ca2a1017f3d973f5466fc6aa542f3602a93d4a4e
Maxime <maxime.jme@gmail.com>
2020-05-17 22:53:17 +0200

    add the basic form to create a new meal booking

--- assets/vue/NewBooking.vue
+++ assets/vue/NewBooking.vue
@@ -1,10 +1,26 @@
 <template>
-    <Datepicker :language="fr"></Datepicker>
+    <form>
+        <div class="form-group">
+            <label for="select_meal" class="required">Choisir un repas</label>
+            <select id="select_meal">
+                <option value="">Choisir un repas</option>
+                <option v-for="meal in meals" :key="meal.id" :value="meal.id">{{meal.title}}</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label class="required">Date du repas</label>
+            <datepicker :language="fr" :full-month-name="true" :required="true" readonly="false" :mondayFirst="true" input-class="form-control"></datepicker>
+        </div>
+        <div class="form-group">
+            <button @click="sendNewBooking" class="btn btn-success">Valider</button>
+        </div>
+    </form>
 </template>
 
 <script>
     import axios from "axios";
     import Datepicker from "vuejs-datepicker"
+    import fr from "vuejs-datepicker/dist/locale/translations/fr"
 
     export default {
         name: "NewBooking",
@@ -13,21 +29,27 @@
         },
         data: function() {
           return {
-              meals: {}
+              meals: {},
+              fr: fr
           }
         },
+        mounted() {
+            this.loadMeals();
+        },
         methods: {
-            loadEntree: function () {
-                this.isLoading = true;
+            loadMeals: function () {
                 axios.get('/api/meals?page=1')
                     .then(response => this.meals = response.data['hydra:member'])
                     .catch(function() {
                         console.log('Erreur dans le chargement!');
                     })
                     .finally(() => {
-                        this.isLoading = false;
+                        // this.isLoading = false;
                     });
             },
+            sendNewBooking: function () {
+                
+            }
         }
     }
 </script>
