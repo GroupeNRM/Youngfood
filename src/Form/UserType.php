e0dd52621151c5ac02478f89b52326c5fb362a76
Nathan <SmashScharrer-Dev@outlook.fr>
2020-05-27 00:24:06 +0200

    Fix Style Bug Delete User Button

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
