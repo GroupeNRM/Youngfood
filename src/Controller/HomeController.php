86dd80421afa314aac5a1d7c225ac0692eab6ee8
Maxime <maxime.jme@gmail.com>
2020-05-26 19:40:48 +0200

    Revert "Move home menu, add "how does youngfood work""

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,17 +10,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home.index")
      */
-    public function home(): Response
+    public function home()
     {
         return $this->render('home/index.html.twig');
-    }
-
-    /**
-     * @return Response
-     * @Route("/comment-ca-marche", name="home.how")
-     */
-    public function howDoesItWork(): Response
-    {
-        return $this->render('home/howItWorks.html.twig');
     }
 }
